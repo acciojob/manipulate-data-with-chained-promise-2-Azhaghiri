2e9e07f9fdc44c75b8df4cde38adc82a34aa820b
Azhaghiri <ganadhan6528@gmail.com>
2025-02-28 08:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-//your JS code here. If required.
 let output = document.getElementById("output");
 
 function manipulateArray() {
-	new Promise((resolve) => {
+	return new Promise((resolve) => {
 		setTimeout(() => {
-			resolve([1, 2, 3, 4])
-		})
+			resolve([1, 2, 3, 4]);
+		}, 1000);
 	})
 	.then((array) => {
 		return new Promise((resolve) => {
 			setTimeout(() => {
-				const eventNumbers = array.filter(num => num%2 == 0);
-				output.innerHTML = eventNumbers.join(", ");
-				resolve(eventNumbers)
-			},1000)
-		})
+				const evenNumbers = array.filter(num => num % 2 === 0);
+				output.innerHTML = evenNumbers.join(", ");
+				resolve(evenNumbers);
+			}, 1000);
+		});
 	})
-	.then((eventNumbers) => {
+	.then((evenNumbers) => {
 		return new Promise((resolve) => {
 			setTimeout(() => {
-				const sum = eventNumbers.map(num => num * 2);
-				output.innerHTML = sum.join(", ")
-				resolve(sum)
-			},3000)
-		})
-	})
+				const doubled = evenNumbers.map(num => num * 2);
+				output.innerHTML = doubled.join(", ");
+				resolve(doubled);
+			}, 2000);
+		});
+	});
 }
+
 manipulateArray();
