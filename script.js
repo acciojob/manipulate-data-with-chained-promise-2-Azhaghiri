c71df4c4ae0688572d523c207a6145eaf51cc6a3
Azhaghiri <ganadhan6528@gmail.com>
2025-02-28 08:03:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let output = document.getElementById("output");
+
+function manipulateArray() {
+	new Promise((resolve) => {
+		setTimeout(() => {
+			resolve([1, 2, 3, 4])
+		},1000)
+	})
+	.then((array) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				const eventNumbers = array.filter(num => num%2 == 0);
+				output.innerHTML = eventNumbers.join(", ");
+				resolve(eventNumbers)
+			},1000)
+		})
+	})
+	.then((eventNumbers) => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				const sum = eventNumbers.map(num => num * 2);
+				output.innerHTML = sum.join(", ")
+				resolve(sum)
+			},3000)
+		})
+	})
+}
+manipulateArray();
